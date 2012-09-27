c5009f72a3a4c3bffb4855f8720ac7b3def2f982
zhonglei yang <zhleiyang@gmail.com>
2012-09-27 20:44:02 +0800

    Add

--- ossClient/ossClient/obj/x86/Debug/OssClientMetro_Content.g.i.cs
+++ ossClient/ossClient/obj/x86/Debug/OssClientMetro_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/right.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/repeat.png")]
 
 
